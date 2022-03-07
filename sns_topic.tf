9cd53e0f8112387018bb6260b5fa9226c4014fb2
Jibin Antony <Jibin.Antony@team.telstra.com>
2022-03-07 19:00:36 +1100

    Updated sns topic name from test to prod

--- sns_topic.tf
+++ sns_topic.tf
@@ -3,7 +3,7 @@ resource "aws_sns_topic" "sns_create_snapshot_backup" {
 }
 
 resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
-  topic_arn = aws_sns_topic.sns_create_snapshot_backup_test.arn
+  topic_arn = aws_sns_topic.sns_create_snapshot_backup.arn
   protocol  = "lambda"
   endpoint  = aws_lambda_function.goodmorning_lambda.arn
 }
